505e1f0e3c9eaf78850f999b469beb3ea17913c9
Oguz Gur <oguz.gur.cs@gmail.com>
2024-08-22 22:21:00 +0100

    number-guesser comments added

--- number-guesser.py
+++ number-guesser.py
@@ -1,40 +1,62 @@
+# Importing random and time modules for:
+# randomizing numbers and to delay responds.
+
 import random
 import time
 
+# Intro of the game when the file executed.
 print("This is a number guessing game!")
-def start_game():
+
+# Main game function that starts the game.
+def main():
+
+    # randrange function randomizes an integer between to numbers.
     guess_number = random.randrange(1, 5)
     time.sleep(1)
     print("Guess from 1-5.")
+
+    # Checks user input with an external function.
     user_number = check_input(input())
 
+    # Checks if user's input equals to the randomized number 
     if guess_number == user_number :
         print("You won!")
         time.sleep(1)
+        # Asks for a new game and restarts the game according to the input.
         print("Do you want to replay? (y/n)")
-        replay = input()
+        restart_game(input())
     else:
-        print("The number was: " + str(guess_number))
-        time.sleep(1)
-        print("Try again!")
+        # If user lost shows the computer's number.
+        print("Try again!" + "\nThe number was: " + str(guess_number))
         time.sleep(1)
         print("Do you want to replay? (y/n)")
-        replay = input()
-    
-    if replay == "y":
-        start_game()
-    else:
-        quit()
+        restart_game(input())
+
 
 def check_input(input_string):
+    # Checks if the input is an integer with .isdigit() function.
     if input_string.isdigit(): 
+        # Checks the number if it's between 1 to 5.
         if int(input_string) > 0 and int(input_string) < 6:
+            # Returns the input value as an integer to the variable
             return int(input_string)
         else:
+            # Gives error as an output then recalls the function with new input.
             print("Error: Number needs to be between 1 to 5.")
             check_input(input())
     else:
-        print("Error: Please type a number!")
+        # Gives error as an output then recalls the function with new input.
+        print("Error: Please type an integer!")
         check_input(input())
 
-start_game()
+# Restarting function to give errors if necessary and runs the main function if needed.
+def restart_game(param):
+    if param == "y":
+        main()
+    elif param == "n":
+        quit()
+    else:
+        print("Error: Please type `y` for yes or `n` for no.")
+        restart_game(input())
+
+main()
